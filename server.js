7aa5d71af31c49422713c8cb3512ad80537cba14
paulohdelia <paulohdelia@gmail.com>
2020-09-30 23:54:37 -0300

    add logic to detect player A and player b

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const server = require('express')();
 const http = require('http').createServer(server);
 const io = require('socket.io')(http);
+let players = [];
 
 io.on('connection', function (socket) {
   console.log('A user connected: ' + socket.id);
 
+  players.push(socket.id);
+
+  if (players.length === 1) {
+    io.emit('isPlayerA');
+  }
+
   socket.on('disconnect', function () {
     console.log('A user disconnected: ' + socket.id);
+    players = players.filter((player) => player !== socket.id);
   });
 });
 
